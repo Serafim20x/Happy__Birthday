daa624438fc2e7f91b5ea1be1b558e1ddfd27839
Serafim20x <sablukov.m@edu.ektu.kz>
2024-06-27 09:26:19 +0500

    Add files via upload

---
+++ Happy_Birthday/script.js
@@ -0,0 +1,7 @@
+
+const desc = document.getElementById("desc")
+const sname = document.getElementById("name")
+
+desc.innerText = "С днём рождения поздравляю!\n И от всей души желаю\n Быть уверенной в себе,\n Быть активной,\n Быть весёлой,\n Никогда не унывать,\n Чтоб была полна душа\n Светом, радостью, добром,\n Чтоб уютным был твой дом.\n Чтоб тобою восхищались,\n Чтоб мечты твои сбывались.";
+
+sname.innerText = "@serafim16x";
